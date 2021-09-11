5772e0dbcf94dd470bf0c9861c855f44828ee0d7
SpekalsG3Viewer <karpick.k@yandex.ru>
2021-09-11 14:49:33 +0300

    updated pm2 resart time to restart every day

--- pm2.config.js
+++ pm2.config.js
@@ -5,6 +5,7 @@ module.exports = {
     script: 'node',
     args: 'dist/src/index.js',
     watch: false,
+    cron_restart: '0 0 * * *',
     max_memory_restart: '1G',
     merge_logs: true,
     log_type: 'json',
