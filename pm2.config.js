decc1f0660a19f33adf27ccbccde9b489ea29e12
Karpik Kirill <karpick.kirill@gmail.com>
2021-11-29 13:57:20 +0300

    upgrade .gitignore and pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -2,10 +2,8 @@ module.exports = {
   apps: [{
     name: 'pepeger',
     cwd: __dirname,
-    script: 'node',
-    args: 'dist/src/index.js',
+    script: 'npm run start',
     watch: false,
-    cron_restart: '0 0 * * *',
     max_memory_restart: '1G',
     merge_logs: true,
     log_type: 'json',
